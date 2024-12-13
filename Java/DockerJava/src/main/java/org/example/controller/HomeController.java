0206e010d604870ca807b14fc9b4af1d6d975944
viana <flood.com@hotmail.com>
2024-12-13 13:55:33 -0300

    commit

---
+++ Java/DockerJava/src/main/java/org/example/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";  // Nome do template Thymeleaf (HTML) que será exibido
+    }
+}
